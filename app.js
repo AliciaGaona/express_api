d3fa061b5dc76e1945bf47177efb06397c009b06
AliciaGaona <aligaovela@gmail.com>
2022-05-03 00:33:35 -0500

    GET Crea un endpoint que regrese una lista de explorers

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ const port = 3000
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
 })
+
+app.get('/v1/explorers', (req, res)=>{
+    console.log(`Api Explorers GET ALL requets ${new Date()}`)
+    const explorer1= {id:1,name:"Carlo1"}
+    const explorer2= {id:2,name:"Cralo2"}  
+    const explorer3= {id:3,name:"Woopa"}
+    const explorer4= {id:4,name:"Woopa1"}
+    const explorers =[explorer1,explorer2,explorer3,explorer4]
+    res.status(200).json(explorers)
+})
