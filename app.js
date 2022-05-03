d227f5dc9517c4b3aa61a27f8c37b671ad35060b
AliciaGaona <aligaovela@gmail.com>
2022-05-03 00:56:14 -0500

    se corrige get con id

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/v1/explorers', (req, res)=>{
-    console.log(`Api Explorers GET ALL requets ${new Date()}`)
-    const explorer1= {id:1,name:"Carlo1"}
-    const explorer2= {id:2,name:"Cralo2"}  
-    const explorer3= {id:3,name:"Woopa"}
-    const explorer4= {id:4,name:"Woopa1"}
-    const explorers =[explorer1,explorer2,explorer3,explorer4]
-    res.status(200).json(explorers)
-})
+// app.get('/v1/explorers', (req, res)=>{
+//     console.log(`Api Explorers GET ALL requets ${new Date()}`)
+//     const explorer1= {id:1,name:"Carlo1"}
+//     const explorer2= {id:2,name:"Cralo2"}  
+//     const explorer3= {id:3,name:"Woopa"}
+//     const explorer4= {id:4,name:"Woopa1"}
+//     const explorers =[explorer1,explorer2,explorer3,explorer4]
+//     res.status(200).json(explorers)
+// })
 
 
 app.get('/v1/explorers/:id', (req, res)=>{
     console.log(`Api Explorers GET ALL requets ${new Date()}`)
     console.log(`Getting explorer with id ${req.params.id}`)
-    const explorer= {id: 1 , name: "Carlo"}
-    res.status(200).json(explorer)
+    const explorers= {id: 1 , name: "Carlo"}
+    res.status(200).json(explorers)
 })
