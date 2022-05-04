6f3bd78fe438c09c32b83ba4353245961c189cf2
AliciaGaona <aligaovela@gmail.com>
2022-05-03 19:48:24 -0500

    POST Crea un endpoint que se encargue de crear un explorer

--- app.js
+++ app.js
@@ -21,10 +21,16 @@ app.get('/v1/explorers', (req, res)=>{
     res.status(200).json(explorers)
 })
 
-
 app.get('/v1/explorers/:id', (req, res)=>{
     console.log(`Api Explorers GET ALL requets ${new Date()}`)
     console.log(`Getting explorer with id ${req.params.id}`)
     const explorer= {id: 1 , name: "Carlo"}
     res.status(200).json(explorer)
+})
+
+
+app.post('/v1/explorers', (req, res)=>{
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body//parametros de un cliente
+    res.status(201).json({message: "Created"})
 })
